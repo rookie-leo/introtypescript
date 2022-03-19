3efb7db458dd49e553fefb13124a7ccd25a4d58c
rookie-leo <proleonarpa@gmail.com>
2022-03-19 10:27:16 -0300

    Utilzando template

--- helloworld.js
+++ helloworld.js
@@ -1,4 +1,5 @@
 var mensagem = "Hello World!";
+var str = "Log diz ".concat(mensagem); //Obrigatóriamente precisa ser o caractere apóstrofo/acento grave
 //console.log recebendo como parâmetro a variável 'mensagem',
 //para exibir o texto associado a ela no console do navegador.
-console.log(mensagem);
+console.log(str);
--- helloworld.ts
+++ helloworld.ts
@@ -1,5 +1,7 @@
 let mensagem: string = "Hello World!";
 
+let str: string = `Log diz ${ mensagem }`;//Obrigatóriamente precisa ser o caractere apóstrofo/acento grave
+
 //console.log recebendo como parâmetro a variável 'mensagem',
 //para exibir o texto associado a ela no console do navegador.
-console.log(mensagem);
+console.log(str);
