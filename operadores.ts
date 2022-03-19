136dc0bda8a86424625890dac924031c46eab09d
rookie-leo <proleonarpa@gmail.com>
2022-03-19 11:19:25 -0300

    Operadores

---
+++ operadores.ts
@@ -0,0 +1,30 @@
+let A: number = 6;
+let B: number = 2;
+
+//resultado da soma
+console.log("A = "+ A);
+console.log("B = "+ B);
+
+//Operador soma (+)
+console.log("A + B = ", A + B);
+
+//Operador de subtração (-)
+console.log("A - B = ", A - B);
+
+//Operador de multiplicação (*)
+console.log("A * B = ", A * B);
+
+//Operador de divisão (/)
+console.log("A / B = ", A / B);
+
+//Operador maior que (>)
+console.log("A > B = ", A > B);
+
+//Operador menor que ( < )
+console.log("A < B = ", A < B);
+
+//Operador AND (&&)
+console.log("A > B && B < A = ", A > B && B < A);
+
+//Operaodr OR (||)
+console.log("A > B || B > A = ", A > B || A < B);
