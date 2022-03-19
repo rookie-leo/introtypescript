ae71ad1c117febfbe4c8251c3e362d1997d7ed4d
rookie-leo <proleonarpa@gmail.com>
2022-03-19 00:25:09 -0300

    Primeira aula de TypeScript

---
+++ helloworld.js
@@ -0,0 +1,4 @@
+var mensagem = "Hello World!";
+//console.log recebendo como parâmetro a variável 'mensagem',
+//para exibir o texto associado a ela no console do navegador.
+console.log(mensagem);
---
+++ helloworld.ts
@@ -0,0 +1,5 @@
+let mensagem: string = "Hello World!";
+
+//console.log recebendo como parâmetro a variável 'mensagem',
+//para exibir o texto associado a ela no console do navegador.
+console.log(mensagem);
