93a9fdbb8f35aa1556ea77dcd3743dd0d0b4e67d
rookie-leo <proleonarpa@gmail.com>
2022-03-19 10:54:02 -0300

    Arrays

---
+++ arrays.js
@@ -0,0 +1,4 @@
+// Arrays
+var numeros = [3, 4, 5];
+var numeros2 = [7, 8, 9];
+console.log(numeros[0]);
---
+++ arrays.ts
@@ -0,0 +1,13 @@
+// Arrays
+let numeros: number[] = [3, 4, 5];
+let numeros2: Array<number> = [7, 8, 9];
+
+// Tuple
+let x:  [string, number];
+x = ["Noel", 65];
+
+// Enum
+enum Cor{Red, Green, Blue};
+let corFundo: Cor = Cor.Blue;
+
+console.log(numeros[0]);
